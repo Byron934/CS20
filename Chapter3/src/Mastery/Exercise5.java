9afa856b417641abf5a27fcb2f8394d90c64ba94
510129014 <510129014@L815WD220025.edu.cbe.ab.ca>
2026-09-22 10:02:30 -0600

    10:02 am

--- Chapter3/src/Mastery/Exercise5.java
+++ Chapter3/src/Mastery/Exercise5.java
@@ -1,6 +1,6 @@
 /*
 
-Program: Exercise5.java          Last Date of this Revision: September 21, 2026
+Program: Exercise5.java          Last Date of this Revision: September 22, 2026
 
 Purpose: Create an application that prompts the user for an dollar ammount (ideally under $1.00)
 and then displays the minumum number of coins necessary to make the change. Displays the
@@ -16,26 +16,27 @@ public class Exercise5 {
 
 	public static void main(String[] args) {
 	
-		//Declare Variables
-		int change, quarters, dimes, nickels, pennies;
+		//Declare variables for the amount of change, total number of coins and each individual coin
+		int change, quarters, dimes, nickels, pennies, totalCoins;
 	    
 	    //Create a Scanner object
 		Scanner userinput = new Scanner(System.in);
 		
-		//Ask the user to enter the amount of change
+		//Prompt the user to enter the amount of change under $1.00
 		System.out.print("Please enter an amount of change less than $1.00: ");
 		
-		//Get the change from the user
+		//Get the change from the user and apply the number to the variable "change"
 		change = userinput.nextInt();
 		
-		//Calculate the minimum amount of coins
+		//Calculate the total minimum amount of coins and the least possible amount of each
 		quarters = change / 25;
 		dimes = change % 25 / 10;
 		nickels = change % 25 % 10 / 5;
 		pennies = change % 25 % 10 % 5 / 1;
+		totalCoins = quarters + dimes + nickels + pennies;
 		
-		
-		//Display the number of coins for the user
+		//Display the total number of coins used and the number of each coin for the user
+		System.out.println("The minimum number of coins is: " + totalCoins);
 		System.out.println("The number of quarters is: " + quarters);
 		System.out.println("The number of dimes is: " + dimes);
 		System.out.println("The number of nickels is: " + nickels);
@@ -47,7 +48,8 @@ public class Exercise5 {
 
 /* Screen Dump
  
- Please enter an amount of change less than $1.00: 99
+Please enter an amount of change less than $1.00: 99
+The minimum number of coins is: 9
 The number of quarters is: 3
 The number of dimes is: 2
 The number of nickels is: 0
@@ -55,6 +57,7 @@ The number of pennies is: 4
 
 
 Please enter an amount of change less than $1.00: 33
+The minimum number of coins is: 5
 The number of quarters is: 1
 The number of dimes is: 0
 The number of nickels is: 1
--- Chapter3/src/Mastery/ObjectHeight.java
+++ Chapter3/src/Mastery/ObjectHeight.java
@@ -1,6 +1,6 @@
 /*
 
-Program: Exercise1.java          Last Date of this Revision: September 21, 2026
+Program: Exercise1.java          Last Date of this Revision: September 22, 2026
 
 Purpose: Create an application that can display the height of an object at any given time 
 that was dropped from a starting height of 100 meters and is accelerating downwards 
@@ -13,28 +13,28 @@ package Mastery;
 
 import java.util.Scanner;
 
-public class Exercise1 {
+public class ObjectHeight {
 
 	public static void main(String[] args) {
 	
-		//Declare Variables
-	    float t, x, h;
+		//Declare variables for time, time to the power of 2, and the height of the object
+	    float time, timeSquared, height;
 	    
 	    //Create a Scanner object
 		Scanner userinput = new Scanner(System.in);
 		
-		//Ask the user to enter the time
+		//Prompt the user to enter the time
 		System.out.print("Please enter a time less than 4.5s: ");
 		
 		//Get the grade from the user
-		t = userinput.nextFloat();
+		time = userinput.nextFloat();
 		
 		//Calculate t^2 first and then the height based on the given time
-		x = t*t;
-		h = 100 -4.9f*x;
+		timeSquared = time*time;
+		height = 100 -4.9f*timeSquared;
 		
-		//Display the height for the user
-		System.out.println("The height of the object is: " + h +"m");
+		//Display the height in meters for the user
+		System.out.println("The height of the object is: " + height +"m");
 		
 	}
 
